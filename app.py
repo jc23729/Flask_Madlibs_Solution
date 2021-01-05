32249556bbf76921f2a62da9ae1757d89678a1f7
Juan Cruz <jc23729@gmail.com>
2021-01-05 00:47:48 -0500

    notes

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import story 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def ask_questions():
+    """Generate and show form to ask words"""
+    prompts = story.prompts
